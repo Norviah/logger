4f72b98bcc7608f279e21541e793d7432fdfce30
zelda <jmasresha@gmail.com>
2020-02-28 00:02:15 -0800

    init: initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Logger
+
+### About
+Logger is a very simple logging system for node.js, it uses [moment](https://momentjs.com) as a dependency in order to get the
+current time of when a log occurs. If wanted, you can also have Logger write logs into a directory, which defaults to `./logs/`,
+with each day having its own file under the name of `MM-DD-YY.txt`.
+
+### Installation
+In order to install and use Logger, install with npm `npm install @norviah/logger`, then require and initialize it in your code:
+```javascript
+const logger = require("@norviah/logger");
+const Logger = new logger({ /** options **/ });
+```
+
+### Usage
+In the previous example, once you have Logger initialized, you can start logging via the `.print` method:
+
+```javascript
+Logger.print("Hello World");
+// => [DD-MM-YY - h:mm a] Hello World
+```
+
+If you want Logger to also save logs into a file, you can initialize it with options, for example:
+
+```javascript
+// assuming that logger is imported as in the example above.
+const Logger = new logger({ write: true });
+```
+
+The `write` key determines if Logger should write logs into a file (`false` by default), there's also another key, `dir`. If the
+`write` key is `true` this key determines what directory should Logger write into (`./logs/` by default). When writing logs into
+a file, Logger will create a new file for the current day with the name `DD-MM-YY.txt`, if a file already exists it will append
+to that file.
+
+In addition, you can also provide a title for the message by doing: `Logger.log(message, title);`:
+
+```javascript
+Logger.print("Hello World!", "Greeting");
+// => [DD-MM-YY - h:mm a] Greeting: Hello World
+```
+
+### Examples
+```javascript
+
+const Logger = new Logger();
+// Logger will only print to the console.
+
+const Logger = new Logger({ write: true });
+// Logger will also write logs into a text file within the directory "./logs".
+
+const Logger = new Logger({ write: true, dir: "./sample text/" );
+// Logger will write logs into the directory "./sample text/", Logger will
+// create the directory if it doesn't exist.
+```
+
+Logger also has a few methods available, for commonly used logging types, `Logger.log(message)`, `Logger.error(message)`,
+`Logger.debug(message)`, and `Logger.warn(message)`, with each having a title representing their names in all caps, for example:
+```javascript
+Logger.warn("Some warning");
+// => [DD-MM-YY - h:mm a] WARNING: Some warning
+```
+
+### License
+[MIT](./LICENSE)
---
+++ main.js
@@ -0,0 +1,120 @@
+// Native imports.
+const fs = require("fs");
+const path = require("path");
+
+// Importing modules.
+const moment = require("moment");
+
+
+class Logger {
+
+  /**
+   * Initializes a new instance, with options.
+   * @param {Object} options Additional options for the logging system.
+   * @param {Boolean} options.write Defaults to false, determines if Logger should also write to a file.
+   * @param {String} options.dir Defaults to "./logs", if options.write is true, this
+   * value represents the directory to where the logs will be saved.
+   */
+
+  constructor(options = {}) {
+
+    // We initialize an object to hold references to
+    // available options and their default values,
+    // along with any overrides that were given.
+    this.options = { write: false, dir: path.join(__dirname, "logs"), ...options };
+
+    // Before we continue, we must make sure that,
+    // if we are writing logs into a file, the directory exists.
+    if (this.options.write && !fs.existsSync(this.options.dir)) {
+      fs.mkdirSync(this.options.dir);
+    }
+
+  }
+
+
+  /**
+   * Logs the given message into the console, if options.write is true,
+   * we also write the message into a file into the given directory,
+   * the name for the file will always be the current date.
+   * @param {String} message The given message to log.
+   * @param {String} title Optional, the logging title.
+   * @return {null} Returns nothing.
+   */
+
+  print(message, title) {
+
+    // We initialize a variable to hold a reference
+    // to the current date, as we'll use this late,
+    const date = moment().format("MM-DD-YY");
+
+    // and we do the same for the time as well.
+    const time = moment().format("h:mm a");
+
+    // Next, we initialie a new variable to hold the
+    // message that we will log, it holds a special format,
+    // the current date, time, the given message, and
+    // the logging title, if a title was given in the first place.
+    message = `[${date} - ${time}] ${title ? `${title}: ` : ""}${message}`;
+
+    console.log(message);
+
+    // After we log the message, we check to see if
+    // the user wants us to also write the message into a file.
+    if (this.options.write) {
+      fs.appendFileSync(`${this.options.dir}/${date}.txt`, `${message}\n`);
+    }
+
+  }
+
+
+  /**
+   * A simple shortcut method to
+   * log an error, the message title is already
+   * set as "ERROR" when using this method.
+   * @param {string} message The given message to log.
+   */
+
+  error(message) {
+    this.print(message, "ERROR");
+  }
+
+
+  /**
+   * A simple shortcut method to
+   * log a warning, the message title is already
+   * set as "WARNING" when using this method.
+   * @param {string} message The given message to log.
+   */
+
+  warn(message) {
+    this.print(message, "WARNING");
+  }
+
+
+  /**
+   * A simple shortcut method to
+   * log a debug, the message title is already
+   * set as "DEBUG" when using this method.
+   * @param {string} message The given message to log.
+   */
+
+  debug(message) {
+    this.print(message, "DEBUG");
+  }
+
+
+  /**
+   * A simple shortcut method to
+   * log a simple logging message, the message
+   * title is already set as "LOG" when using this method.
+   * @param {string} message The given message to log.
+   */
+
+  log(message) {
+    this.print(message, "LOG");
+  }
+
+}
+
+
+module.exports = Logger;
