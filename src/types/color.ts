1468eb3fbd437986d1984c1efd41831f288d3b3a
Norviah <joseph.masresha.481@my.csun.edu>
2022-01-07 19:34:43 -0800

    refactor: refactor codebase

--- src/types/color.ts
+++
@@ -1,7 +0,0 @@
-import { Color } from 'chalk';
-
-/**
- * Represents the colors used for certain parts of a log, the supported colors
- * are the colors that supported by Chalk.
- */
-export type Color = typeof Color;
--- src/types/loggingOptions.ts
+++
@@ -1,9 +0,0 @@
-import { ColorOptions } from './colorOptions';
-import { WriteOptions } from './writeOptions';
-
-export type LoggingOptions = WriteOptions & {
-  /**
-   * Represents colors for different aspects of a log.
-   */
-  colors: ColorOptions;
-};
